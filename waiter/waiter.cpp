f09366b8a9b8dd0fb6f7b52ae3c40a673fc0e60a
Blake Carson <blake.carson.16@cnu.edu>
2019-11-12 14:41:10 -0500

    waiter.cpp Implemented

--- waiter/waiter.cpp
+++ waiter/waiter.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include "stdlib.h"
 
+#include "../includes/externs.h"
 #include "../includes/waiter.h"
 
 using namespace std;
@@ -14,9 +15,27 @@ Waiter::~Waiter()
 
 //gets next Order(s) from file_IO
 int Waiter::getNext(ORDER &anOrder){
-	return SUCCESS;
+	return myIO.getNext(anOrder);
 }
 
 void Waiter::beWaiter() {
+	ORDER currOrder;
+
+	while(getNext(currOrder) == SUCCESS){
+		{
+			unique_lock<mutex> lck(mutex_order_inQ);
+
+			order_in_Q.push(currOrder);
+		}
+
+		cv_order_inQ.notify_all();
+	}
+
+	{
+		unique_lock<mutex> lck(mutex_order_inQ);
+		b_WaiterIsFinished = true;
+	}
+
+	cv_order_inQ.notify_all();
 }
 
