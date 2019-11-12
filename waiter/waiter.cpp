d5ff9758277f088877d9610c18e5ce656b77faae
Blake Carson <blake.carson.16@cnu.edu>
2019-11-12 14:55:33 -0500

    added comments to waiter.cpp

--- waiter/waiter.cpp
+++ waiter/waiter.cpp
@@ -6,6 +6,9 @@
 
 using namespace std;
 
+//ID is just a number used to identify this particular baker
+//(used with PRINT statements)
+//filename is what waiter reads in orders from
 Waiter::Waiter(int id,std::string filename):id(id),myIO(filename){
 }
 
@@ -13,18 +16,25 @@ Waiter::~Waiter()
 {
 }
 
-//gets next Order(s) from file_IO
+//gets next Order from file_IO
+//if return == SUCCESS then anOrder
+//contains new order
+//otherwise return contains fileIO error
 int Waiter::getNext(ORDER &anOrder){
 	return myIO.getNext(anOrder);
 }
 
+//contains a loop that will get orders from filename one at a time
+//then puts them in order_in_Q then signals baker(s) using cv_order_inQ
+//so they can be consumed by baker(s)
+//when finished exits loop and signals baker(s) using cv_order_inQ that
+//it is done using b_WaiterIsFinished
 void Waiter::beWaiter() {
 	ORDER currOrder;
 
 	while(getNext(currOrder) == SUCCESS){
 		{
 			unique_lock<mutex> lck(mutex_order_inQ);
-
 			order_in_Q.push(currOrder);
 		}
 
