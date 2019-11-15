e97dc394547fc56dce831082c0e2cd5d3b98ef0b
Blake Carson <blake.carson.16@cnu.edu>
2019-11-15 11:54:41 -0500

    Tester file changes

--- src/410_proj4.cpp
+++ src/410_proj4.cpp
@@ -117,14 +117,14 @@ int main()
 		std::thread thread_waiter(doWaiter, 1, file);
 
 
+		//Join waiter thread
+		thread_waiter.join();
 
 		//Join baker threads
 		for (auto& baker : baker_threads) {
 			baker.join();
 		}
 
-		//Join waiter thread
-		thread_waiter.join();
 
 		baker_threads.clear();
 
